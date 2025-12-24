d44548b541358bf2b0252f09001c980bb66be2cc
DukicDev <dukic.dev@gmail.com>
2025-12-24 13:47:31 +0100

    Save ports to config file

--- lua/docker_nvim/docker.lua
+++ lua/docker_nvim/docker.lua
@@ -11,6 +11,41 @@ local function create_image_name(project_root)
   return vim.fn.fnamemodify(project_root, ":t") .. "_image"
 end
 
+local config = {
+  port = nil,
+}
+
+local config_file = project.get_project_root() .. "/.docker_nvim.json"
+
+local save_config_to_file = function()
+  local config_data = {
+    port = config.port
+  }
+  local file = io.open(config_file, "w")
+  if file then
+    file:write(vim.fn.json_encode(config_data))
+    file:close()
+  else
+    vim.notify("Error writing to config file.", vim.log.levels.ERROR)
+  end
+end
+
+local load_config_from_file = function()
+  print(config_file)
+  local file = io.open(config_file, "r")
+  if file then
+    local content = file:read("*a")
+    file:close()
+    local config_data = vim.fn.json_decode(content)
+    if config_data then
+      config.port = config_data.port
+    end
+  end
+end
+
+load_config_from_file()
+
+
 function M.build_image(tag)
   local project_root = project.get_project_root()
   if not project_root then
@@ -38,13 +73,17 @@ function M.run(tag, rebuild)
     tag = create_image_name(project_root)
   end
 
-  local ports = vim.fn.input("Ports to map? xxxx:xxxx ")
+  if not config or not config.port then
+    config.port = vim.fn.input("Ports to map? xxxx:xxxx ")
+    save_config_to_file()
+  end
+
   local cmd = ""
   if rebuild then
     cmd = "docker build -t " .. tag .. " " .. project_root .. " && "
   end
   cmd = cmd .. "docker run --name " ..
-      create_container_name(project_root) .. " " .. "-p " .. ports .. ":" .. ports .. " --rm " .. tag
+      create_container_name(project_root) .. " " .. "-p " .. config.port .. ":" .. config.port .. " --rm " .. tag
   terminal.run_command(cmd)
 end
 
