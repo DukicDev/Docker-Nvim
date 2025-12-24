a2497864b06b430e0e243becd9428c04bfaa9f4d
DukicDev <dukic.dev@gmail.com>
2025-12-24 12:01:24 +0100

    Add param for rebuilding container on start

--- lua/docker_nvim/docker.lua
+++ lua/docker_nvim/docker.lua
@@ -19,14 +19,15 @@ function M.build_image(tag)
   end
 
   if not tag or tag == "" then
-    create_image_name(project_root)
+    tag = create_image_name(project_root)
   end
 
   local cmd = "docker build -t " .. tag .. " " .. project_root
   terminal.run_command(cmd)
 end
 
-function M.run(tag)
+function M.run(tag, rebuild)
+  rebuild = rebuild or false
   local project_root = project.get_project_root()
   if not project_root then
     print("No Dockerfile found in project root.")
@@ -34,11 +35,15 @@ function M.run(tag)
   end
 
   if not tag or tag == "" then
-    create_image_name(project_root)
+    tag = create_image_name(project_root)
   end
 
   local ports = vim.fn.input("Ports to map? xxxx:xxxx ")
-  local cmd = "docker run --name " ..
+  local cmd = ""
+  if rebuild then
+    cmd = "docker build -t " .. tag .. " " .. project_root .. " && "
+  end
+  cmd = cmd .. "docker run --name " ..
       create_container_name(project_root) .. " " .. "-p " .. ports .. ":" .. ports .. " --rm " .. tag
   terminal.run_command(cmd)
 end
@@ -79,7 +84,7 @@ function M.shell()
     print("No Dockerfile found in project root.")
     return
   end
-  local cmd = "docker exec -it " .. create_container_name(project_root) .. " /bin/sh"
+  local cmd = "docker exec -it " .. create_container_name(project_root) .. " bash"
   terminal.run_command(cmd, true)
 end
 
